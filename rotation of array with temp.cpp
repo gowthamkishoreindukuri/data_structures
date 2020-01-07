26f78488c5c364e34013a9dce5e70bc6f976e6b0
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-01-07 10:29:01 +0530

    changed

--- rotation of array with temp.cpp
+++ rotation of array with temp.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 int main()
 {
-    cout << "hello world!" << endl;
-return 0;
+    int m,t,n;
+    cout << "enter array size" ;
+    cin >> m ;
+    int a[m];
+    for(int i=0;i<m;i++)
+    {
+        cin >> a[i];
+    }
+    cout << "enter array rotation size" << endl;
+    cin >> n ;
+    for(int j=0;j<n;j++)
+    {
+        t=a[0];
+        for(int k=0;k<m-1;k++)
+        {
+            a[k]=a[k+1];
+        }
+        a[m-1]=t;
+    }
+    for(int l=0;l<m;l++)
+    {
+        cout << a[l] << endl;
+    }
 }
-
