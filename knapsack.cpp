9cbd23815b1ab02d265f0dc2cddf93dca8e6ff8e
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-02-12 07:15:19 +0530

    cera

--- knapsack.cpp
+++ knapsack.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
     int n,m;
-    cin >> n>> m;
+    cin >>n>>m;
     int a[n],b[n],c[n];
     for(int i=0;i<n;i++)
     {
-        cin >> a[i] >>b[i];
+        cin>>a[i]>>b[i];
         c[i]=a[i]/b[i];
     }
 
