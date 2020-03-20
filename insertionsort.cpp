7334da667778381dd6bfc966677e780445eb739f
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-03-20 21:12:24 +0530

    cera

---
+++ insertionsort.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+void printArray(int arr[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+int main()
+{
+    int s;
+    cin>>s;
+    int a[s];
+    for(int i=0;i<s;i++)
+    {
+        cin>>a[i];
+    }
+
+	insertionSort(a, s);
+	printArray(a, s);
+
+	return 0;
+}
+
