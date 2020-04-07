b3d425f006dc654c82e6ae3d04ab2670834e69ff
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-04-07 22:04:15 +0530

    ds

---
+++ stackmain.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class stack
+{
+    int n;
+public:
+    int a[1000];
+    int pop();
+    int push(int x);
+    int peek();
+    bool isempty()
+};
+int stack::pop()
+{
+
+}
+int main()
+{
+    push(x);
+    peek();
+    pop();
+    push();
+    push()
+    isempty();
+    pop();
+    pop();
+    isempty();
+}
