9051504b6a4692a5afcd1e3266ba5bee5182e9fa
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-04-16 21:04:14 +0530

    updated

---
+++ stackinterface.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <algorithm>
+#include <cstdlib>
+using namespace std;
+
+class StackWithMax {
+    vector<int> stack;
+
+  public:
+
+    void Push(int value) {
+        stack.push_back(value);
+    }
+
+    void Pop() {
+        assert(stack.size());
+        stack.pop_back();
+    }
+
+    int Max() const {
+        assert(stack.size());
+        return *max_element(stack.begin(), stack.end());
+    }
+};
+
+int main() {
+    int num_queries = 0;
+    cin >> num_queries;
+
+    string query;
+    string value;
+
+    StackWithMax stack;
+
+    for (int i = 0; i < num_queries; ++i) {
+        cin >> query;
+        if (query == "push") {
+            cin >> value;
+            int nnn=stoi(value);
+            stack.Push(nnn);
+        }
+        else if (query == "pop") {
+            stack.Pop();
+        }
+        else if (query == "max") {
+            cout << stack.Max() << "\n";
+        }
+        else {
+            assert(0);
+        }
+    }
+    return 0;
+}
