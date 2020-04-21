7c7d78579482b88c1d2e5b9fb33ec1353df401f8
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-04-21 22:03:50 +0530

    ds

---
+++ stackarraydym.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define length 4
+#define bound 4
+int create()
+{
+    //for(int )
+}
+int peek()
+{
+
+}
+int push()
+{
+
+}
+int pop()
+{
+
+}
+int main()
+{
+   push(2)
+   pop();
+   peek();
+}
