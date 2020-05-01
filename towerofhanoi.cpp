cfe0ecf2cc8e25690ed98334f1c160c0a4aaa22c
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-05-01 22:46:12 +0530

    adding more

---
+++ towerofhanoi.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int toh(int n,int a,int b,int c)
+{
+
+}
+int main()
+{
+    int n;
+    cout<<" no of disks =";
+    cin>>n;
+    toh(n,1,2,3);
+    return 0;
+}
