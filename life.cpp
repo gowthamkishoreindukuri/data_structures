cdd729d9a18e1aa9dddfeab1aed3d5abf52c264d
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-06-06 09:23:32 +0530

    ud

---
+++ life.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    while(1)
+    {
+        int t;
+        cin>>t;
+        if(t!=42)
+        {
+            cout<<t<<endl;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
